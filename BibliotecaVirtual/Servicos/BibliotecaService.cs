2fb427830d8385de137573a46c4166f115bfc89c
Eduardo <eteixeira566@gmail.com>
2025-05-19 10:41:18 -0300

    commit

--- BibliotecaVirtual/Servicos/BibliotecaService.cs
+++ BibliotecaVirtual/Servicos/BibliotecaService.cs
@@ -1,8 +1,36 @@
 ﻿
 
+using BibliotecaVirtual.Dominio;
+
 namespace BibliotecaVirtual.Servicos
 {
     internal class BibliotecaService
     {
+        private List<Livro> livros = new List<Livro>();
+
+        public void CadastrarLivros(string titulo, string autor, int ano)
+        {
+            if (livros.Any(l => l.Titulo.ToLower() == titulo.ToLower()))
+            {
+                Console.WriteLine("Livro já cadastrado.");
+                return;
+            }
+
+            livros.Add(new Livro(titulo, autor, ano));
+            Console.WriteLine("Livro cadastrado com sucesso!");
+        }
+
+        public void ListarLivros()
+        {
+            if (!livros.Any())
+            {
+                Console.WriteLine("Nenhum livro cadastrado.");
+                return;
+            }
+        }
+
+
+
+
     }
 }
