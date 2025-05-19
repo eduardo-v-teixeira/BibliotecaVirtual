a45affc8e9aad0934713b89f9fd8c23fdf48f0e1
Eduardo <eteixeira566@gmail.com>
2025-05-19 10:21:34 -0300

    commit

--- BibliotecaVirtual/Dominio/Livro.cs
+++ BibliotecaVirtual/Dominio/Livro.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BibliotecaVirtual.Dominio
 {
-    internal class Livro
+    public class Livro
     {
+        public string Titulo { get; private set; }
+        public string Autor { get; private set; }
+        public int Ano { get; private set; }
+        public bool Emprestado { get; private set; }
+
+        public Livro(string titulo, string autor, int ano)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Ano = ano;
+            Emprestado = true;
+        }
+
     }
 }
